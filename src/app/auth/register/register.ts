249163813326c52252f94222d70fcb5ab07e16dd
Nicor-r <nicolasrocha@unicauca.edu.co>
2026-09-16 09:57:41 -0500

    Agregue campos que faltaban en el registro

--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -14,8 +14,11 @@ import { RouterLink } from '@angular/router';
 function passwordsCoincidenValidator(): ValidatorFn {
   return (group: AbstractControl): ValidationErrors | null => {
     const password = group.get('password')?.value;
-    const password2 = group.get('password2')?.value;
-    return password === password2 ? null : { passwordsNoCoinciden: true };
+    const confirmPassword = group.get('confirmPassword')?.value;
+
+    return password === confirmPassword
+      ? null
+      : { passwordsNoCoinciden: true };
   };
 }
 
@@ -34,12 +37,12 @@ export class Register {
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group(
       {
-        nombre: ['', [Validators.required]],
+        nombreCompleto: ['', [Validators.required]],
         email: ['', [Validators.required, Validators.email]],
-        identificacion: ['', [Validators.required]],
-        telefono: ['', [Validators.required]],
+        numeroIdentificacion: ['', [Validators.required]],
+        numeroTelefonico: ['', [Validators.required]],
         password: ['', [Validators.required, Validators.minLength(8)]],
-        password2: ['', [Validators.required]],
+        confirmPassword: ['', [Validators.required]],
         acepta: [false, [Validators.requiredTrue]],
       },
       { validators: passwordsCoincidenValidator() }
@@ -57,7 +60,7 @@ export class Register {
   get passwordsNoCoinciden(): boolean {
     return (
       this.registerForm.hasError('passwordsNoCoinciden') &&
-      !!this.registerForm.get('password2')?.touched
+      !!this.registerForm.get('confirmPassword')?.touched
     );
   }
 
@@ -66,6 +69,7 @@ export class Register {
       this.registerForm.markAllAsTouched();
       return;
     }
+
     // TODO: llamar al AuthService una vez lo creemos
   }
 }
